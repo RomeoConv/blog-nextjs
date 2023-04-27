16ff91bcc5b2d981c91259d9847ae4a7df42d3e2
AConvert <68063405+AConvert@users.noreply.github.com>
2023-04-27 16:23:16 +0100

    adding links into the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,30 @@
 import Image from "next/image";
+import Link from "next/link";
+import { BellIcon } from "@heroicons/react/24/solid";
 
 export default function Navbar() {
   return (
     <main className="nav_main">
-      <section>
-        <Image src={"/transp_logo.png"} height={60} width={90} alt="nav_logo" />
+      <section className="nav_container">
+        <div className="nav_left">
+          <Image
+            src={"/transp_logo.png"}
+            height={60}
+            width={120}
+            alt="nav_logo"
+          />
+          <div className="nav_links">
+            <Link className="link" href="/about">
+              ABOUT
+            </Link>
+            <Link className="link" href="/subscribe">
+              SUBSCRIBE
+            </Link>
+          </div>
+        </div>
+        <div className="nav_right">
+          {/* <BellIcon className="notify_icon" /> */}
+        </div>
       </section>
     </main>
   );
